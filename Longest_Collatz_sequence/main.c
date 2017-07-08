baaa43f23fa395ab15926ebe725154362e8ddfe9
Cocoyb <tiantang520.0@gmail.com>
2017-07-08 22:01:31 +0430

    first finish cost 15 minutes

--- Longest_Collatz_sequence/main.c
+++ Longest_Collatz_sequence/main.c
@@ -1,7 +1,132 @@
+/*
 #include "stdio.h"
 #include "string.h"
+#include "stdbool.h"
+#define MaxInteger 1000000
+typedef unsigned int Uint32;
+
+bool IsEven(Uint32 number);
+Uint32 EvenReturnNum(Uint32 number);
+bool IsOdd(Uint32 number);
+Uint32 OddReturnNum(Uint32 number);
+
 int main()
 {
 	printf("hello world\n");
+	Uint32  CurInterger;
+	Uint32 Temporary_variable;
+	Uint32 gCount = 0, Count = 0;
+	Uint32 gResultNum = 0, ResultNum = 0;
+	for (CurInterger = MaxInteger; CurInterger > 0; CurInterger--)
+	{
+		if (IsEven(CurInterger) == true) 
+		{
+			continue;
+		}
+		printf("\nCurInterger=%u  ", CurInterger);
+		gCount = 1;
+		// dispose current interger
+		gResultNum = CurInterger;
+		Temporary_variable = gResultNum;
+		while (Temporary_variable != 1)
+		{
+			
+			//Temporary_variable = gResultNum;
+			printf("%d--", Temporary_variable);
+			if (IsEven(Temporary_variable))
+			{
+				gCount++;
+				Temporary_variable = EvenReturnNum(Temporary_variable);
+			}
+			else
+			{
+				gCount++;
+				Temporary_variable = OddReturnNum(Temporary_variable);
+			}
+		}
+		if (gCount > Count)
+		{
+			
+			Count = gCount;
+			ResultNum = gResultNum;
+		}
+			
+	}
+
+	printf("\n%u has the longest chain ,count = %u\n", ResultNum, Count);
+    getchar();
+    return 0;
+}
+bool IsEven(Uint32 number)
+{
+	bool flag = false;
+	if (number % 2 == 0)
+	{
+		flag = true;
+	}
+	return flag;
+}
+Uint32 EvenReturnNum(Uint32 number)
+{
+	Uint32 num = 0;
+	num = number / 2;
+	return num;
+}
+
+bool IsOdd(Uint32 number)
+{
+	bool flag = false;
+	if (number % 2 == 0)
+	{
+		return flag;
+	}
+	flag = true;
+	return flag;
+}
+Uint32 OddReturnNum(Uint32 number)
+{
+	Uint32 num = 0;
+	num = 3 * number + 1;
+	return num;
+}
+*/
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int longest = 0;
+	int terms = 0;
+	int i;
+	unsigned long j;
+
+	//for (i = 1; i <= 1000000; i++)
+	//{
+		i=j = 500;
+		int this_terms = 1;
+
+		while (j != 1)
+		{
+			this_terms++;
+
+			if (this_terms > terms)
+			{
+				terms = this_terms;
+				longest = i;
+			}
+
+			if (j % 2 == 0)
+			{
+				j = j / 2;
+			}
+			else
+			{
+				j = 3 * j + 1;
+			}
+		}
+	//}
+
+	printf("longest: %d (%d)\n", longest, terms);
 	getchar();
+	return 0;
 }
